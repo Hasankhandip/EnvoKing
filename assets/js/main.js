7eae536b4200aeb2e40a09aff60afeb30f8f8e1b
Dip <hasankhandip@gmail.com>
2021-12-19 23:45:27 +0600

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+// slick slider
+$(document).ready(function(){
+    $('.banner-slider').slick({
+      prevArrow: '<a href="#0" class="prev"><i class="las la-long-arrow-alt-left"></i></a>',
+      nextArrow: '<a href="#0" class="next"><i class="las la-long-arrow-alt-right"></i></a>',
+    });
+  });
