2a7c106dc23563eae4c39864ee8add97cd190424
Dip <hasankhandip@gmail.com>
2021-12-20 23:46:24 +0600

    footer add

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,37 @@
+// header-section
+$(window).on("scroll", function(){
+  if ($(this).scrollTop() > 170 ) {
+    $(".header-bottom").addClass("active");
+    $(".header-top").addClass("remove");
+    $(".header-logo").addClass("remove");
+    $(".header-logo-2").addClass("active");
+    $(".nav-link").addClass("active-2");
+    $(".active-2-active").addClass("activated");
+    $(".nav-link i").addClass("active");
+  } else {
+    $(".header-bottom").removeClass("active");
+    $(".header-top").removeClass("remove");
+    $(".header-logo").removeClass("remove");
+    $(".header-logo-2").removeClass("active");
+    $(".nav-link").removeClass("active-2");
+    $(".active-2-active").removeClass("activated");
+    $(".nav-link i").removeClass("active");
+  }
+});
+
+// scroll-top btn start
+$(window).on("scroll", function () {
+  if ($(this).scrollTop() > 300) {
+    $(".scroll-top").addClass("active");
+  } else {
+    $(".scroll-top").removeClass("active");
+  }
+});
+
+$(".scroll-top").on("click", function () {
+  $("html, body").animate({ scrollTop: 0 }, 500);
+});
+// scroll-top btn end
 // slick slider
 $(document).ready(function(){
     $('.banner-slider').slick({
